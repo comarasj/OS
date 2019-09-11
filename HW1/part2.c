1fba4a1c16a9410edfc4644b27a093da6d914845
Stephen Comarata <comarasj@mail.uc.edu>
2019-09-11 15:16:44 -0400

    edit to part2

--- HW1/part2.c
+++ HW1/part2.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include <sys/types.h>
-
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
+	char* file_name;
 	if (argc == 1) {
 		printf("Invalid command line syntax, run program like:\npart2 <file_name>\nWhere file_name is a file ");
-
+		return 0;
 	}else{
-		printf("%s\n", argv[1]);
+		file_name = argv[1];
+		printf("%s\n", file_name);
+		processes(file_name);
 	}
+	return 0;
 
-
+}
+void processes(char* file_name ) {
+	int pid0 = fork();
+	if (pid0 == 0){
+		//child 0
+		execl("/bin/ls", "ls", "-l", (char*) NULL);
+	}else{
+		int pid1 = fork();
+		if (pid1 == 0){
+			//child 1
+			execl("/bin/ps", "ps", "-ef", (char*) NULL);
+		}else{
+			int pid2 = fork();
+			if(pid2 == 0){
+				execl("/bin/more", "more", file_name, (char*) NULL);
+			}else{
+				printf("This is the main process, my PID is %d\n", getpid());
+				wait();
+				wait();
+				wait();
+				printf("main process terminates\n");
+			}
+		}
+	}
 }
