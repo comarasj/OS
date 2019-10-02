e86638f35ee0d28956cab61efcc771494566aafc
Stephen Comarata <comarasj@mail.uc.edu>
2019-10-02 15:01:17 -0400

    addign parts 4 & 5

--- HW2/part4_client.c
+++ HW2/part4_client.c
@@ -4,7 +4,60 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/shm.h>
 
-int main( int argc, char* argv[]) {
+const int SHM_SIZE = 1024;  /* make it a 1K shared memory segment */
+const char FILE_NAME[] = "shared.dat";
+const int MAXLINE = 255;
 
+int main(int argc, char *argv[])
+{
+	key_t key;
+	int shmid;
+	char *data;
+	char line[MAXLINE];
+	int n;
+	FILE *fp;
+
+	/* make the key: */
+	if ((key = ftok(FILE_NAME, 1)) == -1) {
+		perror("ftok");
+        	return 0;
+    	}
+
+   	 /* connect to (and possibly create) the segment: */
+   	 if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1) {
+        	perror("shmget");
+        	return 0;
+    	}
+ 	/* attach to the segment to get a pointer to it: */
+	data = shmat(shmid, (void *)0, 0);
+    	if (data == (char *)(-1)) {
+        	perror("shmat");
+        	return 0;
+	}
+
+	/* modify the segment, based on the command line: */
+	fp = fopen("part3.txt", "r");
+	while (fgets(line, MAXLINE, fp) != NULL){
+		for (int i = 0; i < MAXLINE; i++){
+			if (line[i] >= 'a' && line[i] <= 'z'){
+				line[i] = line[i] - 32;
+			}
+		}
+		strncpy(data, line, SHM_SIZE);
+		printf("%s", line);
+		sleep(1);
+	}
+	fclose(fp);
+
+	strncpy(data, "STOP", SHM_SIZE);
+
+	/* detach from the segment: */
+	if (shmdt(data) == -1) {
+        	perror("shmdt");
+        	return 0;
+	}
+	return 0;
 }
+
--- HW2/part4_server.c
+++ HW2/part4_server.c
@@ -4,7 +4,53 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/shm.h>
 
-int main( int argc, char* argv[]) {
+const int SHM_SIZE = 1024;  /* make it a 1K shared memory segment */
+const char FILE_NAME[] = "shared.dat";
 
+int main(int argc, char *argv[])
+{
+    key_t key;
+    int shmid;
+    char *data;
+    int n;
+
+    /* make the key: */
+    if ((key = ftok(FILE_NAME, 1)) == -1) {
+        perror("ftok");
+        return 0;
+    }
+
+    /* connect to (and possibly create) the segment: */
+    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1) {
+        perror("shmget");
+        return 0;
+    }
+    /* attach to the segment to get a pointer to it: */
+    data = shmat(shmid, (void *)0, 0);
+    if (data == (char *)(-1)) {
+        perror("shmat");
+        return 0;
+    }
+
+    /* read the segment */
+    n = 0;
+    while ( (strcmp(data, "STOP") != 0) )
+    {
+        printf("%s", data);
+        sleep(1);   /* Sleep 1 seconds) */
+    }
+
+    /* detach from the segment: */
+    if (shmdt(data) == -1) {
+        perror("shmdt");
+        return 0;
+    }
+
+    /* delete the IPC structure */
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
 }
+
+
